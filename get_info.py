399c5d1285b77ccbd13e167ba6019c2a2784adf5
nle244 <nle244@csu.fullerton.edu>
2022-04-27 16:38:16 -0700

    Added RNG to compare.py for now

--- get_info.py
+++ get_info.py
@@ -34,44 +34,6 @@ def get_info(link):
         print(each.text)
 
 
-    tree = ET.parse('animelist.xml')
-    root = tree.getroot()
-
-    anime = ET.Element("ANIME")
-    item1 = ET.SubElement(anime, 'title')
-    item1.text = title
-
-    item2 = ET.SubElement(anime, 'score')
-    item2.text = score
-
-    item3 = ET.SubElement(anime, 'studio')
-    item3.text = studio
-
-    x = etree.parse(root)
-    pretty_xml= etree.tostring(x, pretty_print=True, encoding=str)
-    print(pretty_xml)
-
-    
-    ugly = ET.tostring(anime, 'utf-8')
-    pretty = minidom.parseString(ugly).toprettyxml(indent="\t")
-    root.append(anime)
-    ET.dump(tree)
-    # # with open("animelist.xml", "w") as f:
-    # #     f.write(pretty)
-
-    # with open("animelist.xml", 'w') as output:
-    #     output.write(pretty)
-
-
-    #tree.write("animelist.xml", encoding="utf-8")
-    # ugly = ET.tostring(tree, 'utf-8')
-    # pretty = minidom.parseString(ugly)
-    # print(pretty.toprettyxml(indent="\t"))
-
-    # file1 = open("animelist.xml", 'a')
-    # file1.writelines(title)
-    # file1.close()
-
 
 def main():
     get_info('https://myanimelist.net/anime/48736/Sono_Bisque_Doll_wa_Koi_wo_Suru')
